3a23c0d8d3f24a976930bc5da4f68b60e62b6096
suhailakhtar039 <suhailakhtar039@gmail.com>
2019-01-17 20:12:10 +0530

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,9 +15,25 @@ io.on("connection",(socket)=>{
 	socket.on("disconnect",()=>{
 		console.log("client disconnected");
 	})
+	socket.emit('newMessage',{
+		from:'Admin',
+		text:'Welcome to the chat app',
+		createdAt:new Date().getTime()
+	});
+	socket.broadcast.emit('newMessage',{
+		from:'Admin',
+		text:'New user joined',
+		createdAt:new Date().getTime()
+	});
 	socket.on("createMessage",(message)=>{
 		console.log("Create message",message); 
-		io.emit('newMessage',{
+		// io.emit('newMessage',{  //It will emit the events to each and every client including
+		// 	from:message.from,	   // this
+		// 	text:message.text,
+		// 	createdAt:new Date().getTime()
+		// });
+		//===It will emit the events to each and every client excluding this
+		socket.broadcast.emit("newMessage",{
 			from:message.from,
 			text:message.text,
 			createdAt:new Date().getTime()
