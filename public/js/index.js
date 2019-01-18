21bd1459faa6c28541730db290879a03151efda9
suhailakhtar039 <suhailakhtar039@gmail.com>
2019-01-18 17:27:39 +0530

    scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket=io();
+function scrollToBottom(){
+	var messages=jQuery("#messages");
+	var newMessage=messages.children('li:last-child');
+
+	var clientHeight=messages.prop('clientHeight');
+	var scrollTop=messages.prop("scrollTop");
+	var scrollHeight=messages.prop("scrollHeight");
+	var newMessageHeight=newMessage.innerHeight();
+	var lastMessageHeight=newMessage.prev().innerHeight();
+
+	if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+}
 socket.on("connect",function(){
 	console.log("connected to server");
 })
@@ -14,6 +28,7 @@ socket.on("newMessage",function(message){
 		createdAt:formattedTime
 	});
 	jQuery("#messages").append(html);
+	scrollToBottom();
 })
 
 socket.on("newLocationMessage",function(message){
@@ -25,6 +40,7 @@ socket.on("newLocationMessage",function(message){
 		createdAt:formattedTime
 	})
 	jQuery("#messages").append(html);
+	scrollToBottom();
 })
 
 jQuery("#message-form").on("submit",function(e){
